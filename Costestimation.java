b9bb3f3cdfdfdafde332ee591bcc55d59224fd57
sanjana <nallasanjanareddy0514@gmail.com>
2020-02-20 13:32:39 +0530

    Add files via upload

---
+++ Costestimation.java
@@ -0,0 +1,30 @@
+package Hometask4;
+
+public class Costestimation {
+		double calculateEstimate(double area, int material_standard, int automatic)
+		 {
+		  double total_cost;
+		  if(automatic == 0)
+		  {
+		   if(material_standard ==1)
+		   {
+		    total_cost=1200*area;
+		   }
+		   else if(material_standard == 2)
+		   {
+		    total_cost = 1500*area;
+		   }
+		   else
+		   {
+		    total_cost = 1800*area;
+		   }
+		  }
+		  else
+		  {
+		   total_cost = 2500*area;
+		  }
+		 return total_cost;
+		 }
+		}
+
+
